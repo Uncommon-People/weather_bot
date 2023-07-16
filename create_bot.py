1c23ed6f6039a12aa97d6c88fe90892fc93d91d3
dmikrit <mai.grachev.dmitriy@gmail.com>
2023-07-16 04:23:27 +0300

    Test

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,8 @@
+import json
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
 
-TOKEN = ''
+TOKEN = json.load(open('keys.json'))['TOKEN']
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
